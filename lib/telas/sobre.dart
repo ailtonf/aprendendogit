703f068cf2adbda945e835bce0fb3b165966084a
Ailton <f.futuretechcode@gmail.com>
2020-05-02 21:36:14 -0400

    Tela sobre terminada - Tininho

---
+++ lib/telas/sobre.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Sobre extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Oiii, tela sobre aqui!"),
+      
+    );
+  }
+}
